9f165f576b14cdc6b48e3d18ddf871766c202914
NUR ROHADATUL AISY BINTE ROZMIN 2105231A <2105231A@student.tp.edu.sg>
2024-01-15 10:11:14 +0800

    latest one sharring with Hiran

--- FeastFoodDelivery/Server/Models/ApplicationUser.cs
+++ FeastFoodDelivery/Server/Models/ApplicationUser.cs
@@ -4,5 +4,7 @@ namespace FeastFoodDelivery.Server.Models
 {
     public class ApplicationUser : IdentityUser
     {
+        public string? FirstName { get; set; }
+        public string? LastName { get; set;}
     }
 }
---
+++ FeastFoodDelivery/Shared/Domain/Customer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FeastFoodDelivery.Shared.Domain
+{
+	public class Customer : BaseDomainModel
+	{
+		public string? CustName { get; set; }
+        public string? Address { get; set; }
+		public string? Email { get; set; }
+		public int MobileNumber { get; set; }
+
+	}
+}
